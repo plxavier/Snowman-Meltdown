d5a5f0bbabbe824511d55fb87423642fe7e7117d
Paul Lourdu Xavier <34255718+plxavier@users.noreply.github.com>
2025-10-01 15:51:54 +0200

    more melting stages

--- ascii_art.py
+++ ascii_art.py
@@ -1,29 +1,50 @@
-# Snowman ASCII Art stages
+# Snowman ASCII Art stages - with more stages
 STAGES = [
-     # Stage 0: Full snowman
-     """
+    # Stage 0: Full snowman
+    """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
      ( : ) 
      """,
-     # Stage 1: Bottom part starts melting
-     """
+    # Stage 1: Slight melting
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     (   ) 
+     """,
+    # Stage 2: Bottom part melting
+    """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
      """,
-     # Stage 2: Only the head remains
-     """
+    # Stage 3: Body shrinking
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     (   ) 
+     """,
+    # Stage 4: Only head remains
+    """
       ___  
      /___\\ 
      (o o) 
      """,
-     # Stage 3: Snowman completely melted
-     """
+    # Stage 5: Head melting
+    """
+      ___  
+     /___\\ 
+     (   ) 
+     """,
+    # Stage 6: Snowman completely melted
+    """
       ___  
      /___\\ 
      """
- ]
+]
